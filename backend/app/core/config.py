1ceae61d93ef3c6381392e4b3a806e9c13c02303
navinexpo <navinexpo2020@gmail.com>
2026-07-02 10:57:18 +0530

    Configuration for Pydantic settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     GROQ_API_KEY: str
     PROJECT_NAME: str = "VoxBrief"
-
+     # Configuration for Pydantic settings
     class Config:
         env_file = ".env"
         extra = "ignore"
