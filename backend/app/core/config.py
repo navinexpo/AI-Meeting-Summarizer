38bdca3d9cf0f5d9bcd1e5e3bace677a0ff6d67a
navinexpo <navinexpo2020@gmail.com>
2026-07-15 23:23:41 +0530

    locating configuration files

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,14 +1,13 @@
 import os
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+# Define the base directory of the project by resolving the path of the current file and navigating up three levels. This is useful for locating configuration files and other resources relative to the project's root directory.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-# Load environment variables from a .env file located in the base directory
+
 class Settings(BaseSettings):
     GROQ_API_KEY: str
     PROJECT_NAME: str = "VoxBrief"
 
-    # Pydantic settings configuration to specify the .env file location and encoding
     model_config = SettingsConfigDict(
         env_file=os.path.join(BASE_DIR, ".env"),
         env_file_encoding="utf-8",
