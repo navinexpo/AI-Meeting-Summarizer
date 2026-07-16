539d646f3b6a2b28e9df950db1ba6f707454eda4
navinexpo <navinexpo2020@gmail.com>
2026-07-16 14:22:05 +0530

    Set Extra Parameter

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 class Settings(BaseSettings):
     GROQ_API_KEY: str
     PROJECT_NAME: str = "VoxBrief"
-
+# Set the configuration for the Settings class, specifying the location of the environment file and its encoding. The 'extra' parameter is set to "ignore" to allow for additional environment variables that are not explicitly defined in the Settings class.
     model_config = SettingsConfigDict(
         env_file=os.path.join(BASE_DIR, ".env"),
         env_file_encoding="utf-8",
